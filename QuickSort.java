68c4c64975b3679d820f1364718507e1aa9fac1b
NFUE <37241785+cptbaker233@users.noreply.github.com>
2019-04-15 19:21:24 +0800

    Add files via upload

--- QuickSort.java
+++ QuickSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Random;
 
 /**  
-* @typename QuickSort_Retry01  
+* @typename QuickSort 
 * @author NFUE  
 * @Description: 快速排序算法
 * @date 2019年4月9日 下午9:04:10    
@@ -14,38 +14,35 @@ import java.util.Random;
 public class QuickSort {
     public static void main(String[] args) {
         Random r = new Random();
-        int[] arr = new int[1000];
+        int[] arr = new int[10];
         for (int i = 0; i < arr.length; i ++) {
             arr[i] = r.nextInt(100);
         }
         //int[] arr = {0,6,10,5,5,5,10};
         System.out.println(Arrays.toString(arr));
+        long start = System.currentTimeMillis();
         arr = quickSort(arr);
+        long time = System.currentTimeMillis() - start;
         System.out.println(Arrays.toString(arr));
+        System.out.println("用时:" + time);
+        System.out.println(arr.length);
     }
     
     //快速排序算法
     public static int[] quickSort(int[] arr) {
-        return quickSortPart(arr, 0, arr.length - 1);
+        return quickSort(arr, 0, arr.length - 1);
     }
     
     //局部快排算法
-    public static  int[] quickSortPart(int[] arr, int start, int end) {
+    public static  int[] quickSort(int[] arr, int start, int end) {
         if (end - start == 0) {
-            return arr;
-        }
-        if (end - start == 1) {
-            if (arr[start] > arr[end]) {
-                int temp = arr[start];
-                arr[start] = arr[end];
-                arr[end] = temp;
-            }
             return arr;
         }
         int s = start;
         int e = end;
         int key = s;
         while (s < e) {
+            
             if (key == s) {
                 if (arr[e] < arr[key]) {
                     int temp = arr[e];
@@ -55,7 +52,7 @@ public class QuickSort {
                     continue;
                 }
                 e --;
-                continue;
+                continue; 
             }
             if (key == e) {
                 if (arr[s] > arr[key]) {
@@ -70,13 +67,18 @@ public class QuickSort {
             }
         }
         if (key == start) {
-            return quickSortPart(arr, key + 1, end);
+            return quickSort(arr, key + 1, end);
         }
         if (key == end) {
-            return quickSortPart(arr, start, key - 1);
+            return quickSort(arr, start, key - 1);
         }
-        quickSortPart(arr, start, key - 1);
-        quickSortPart(arr, key + 1, end);
+        System.out.println(Arrays.toString(arr));
+        quickSort(arr, start, key - 1);
+        System.out.println("左边排序后: ");
+        System.out.println(Arrays.toString(arr));
+        quickSort(arr, key + 1, end);
+        System.out.println("右边排序后: ");
+        System.out.println(Arrays.toString(arr));
         return arr;
     }
 }
