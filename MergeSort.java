4e7a34f039dd57ec3a2643cd9c3573912e6a3b2b
NFUE <37241785+cptbaker233@users.noreply.github.com>
2019-04-29 20:46:35 +0800

    Add files via upload

--- MergeSort.java
+++ MergeSort.java
@@ -1,70 +1,73 @@
 package com.igeek.归并排序法;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Random;
 
 public class MergeSort {
     public static void main(String[] args) {
         Random r = new Random();
-        int[] arr = new int[30];
-        for (int i = 0; i < arr.length; i ++) {
-            arr[i] = r.nextInt(90) + 10;
+        int[] arr = new int[100000];
+        for (int i = 0; i < arr.length; i ++) { 
+            arr[i] = r.nextInt(100000) + 1;
         }
         System.out.println("排序前:\n" + Arrays.toString(arr));
-        ArrayList<Integer> al = new ArrayList<Integer>();   //为了方便操作,将数组存入集合
+        LinkedList<Integer> ll = new LinkedList<Integer>();   //为了方便操作,将数组存入集合
         for (int i = 0; i < arr.length; i ++) {
-            al.add(arr[i]);
+            ll.add(arr[i]);
         }
-        al = merge(al);
+        long start = System.currentTimeMillis();
+        ll = merge(ll);
+        long time = System.currentTimeMillis() - start;
         for (int i = 0; i < arr.length; i ++) {             //排序完成后把数列重新存进数组输出
-            arr[i] = al.get(i);
+            arr[i] = ll.get(i);
         }
         System.out.println("排序后:\n" + Arrays.toString(arr));
+        System.out.println("用时:" + time);
+        System.out.println(arr.length);
     }
   
     //归并排序算法
-    public static ArrayList<Integer> merge(ArrayList<Integer> al) {
-        if (al.size() == 1) {       //递归出口,如果分无可分则不继续分,直接返回集合
-            return al;
+    public static LinkedList<Integer> merge(LinkedList<Integer> ll) {
+        if (ll.size() == 1) {       //递归出口,如果分无可分则不继续分,直接返回集合
+            return ll;
         }
-        ArrayList<Integer> al1 = new ArrayList<Integer>();
-        ArrayList<Integer> al2 = new ArrayList<Integer>();
-        for (int i = 0; i < al.size(); i ++) {              //将目标集合平均分成两组
-            if (i < al.size() / 2) {
-                al1.add(al.get(i));
+        LinkedList<Integer> ll1 = new LinkedList<Integer>();
+        LinkedList<Integer> ll2 = new LinkedList<Integer>();
+        for (int i = 1; ll.size() > 0;) {
+            if (i == 1) {
+                ll1.addLast(ll.removeLast());
+                i = 0;
             } else {
-                al2.add(al.get(i));
+                ll2.addLast(ll.removeLast());
+                i = 1;
             }
         }
-        al1 = merge(al1);           //利用递归使第一组有序
-        System.out.println("A组" + al1);
-        al2 = merge(al2);           //利用递推使第二组有序
-        System.out.println("B组" + al2);
-        al = merg(al1, al2);        //输入有序的两个集合,使合并后的集合有序
-        System.out.println("合并:" + al);
-        return al;
+        ll1 = merge(ll1);           //利用递归使第一组有序
+        ll2 = merge(ll2);           //利用递推使第二组有序
+        ll = merg(ll1, ll2);        //输入有序的两个集合,使合并后的集合有序
+        return ll;
      }
     //两个有序数列,使合并后有序
-    public static ArrayList<Integer> merg(ArrayList<Integer> al1, ArrayList<Integer> al2) {
-        ArrayList<Integer> result = new ArrayList<Integer>();
+    public static LinkedList<Integer> merg(LinkedList<Integer> al1, LinkedList<Integer> al2) {
+        LinkedList<Integer> result = new LinkedList<Integer>();
         while (al1.size() != 0 || al2.size() != 0) {
             if (al1.size() == 0 ) {                 //实际上归并排序不会出现的情况,归并一般平均分组
-                while (al2.size() != 0 ) {          //这里是为了提升本方法的适用场景,使不平均分组的情况也适用
-                    result.add(al2.remove(0));
+                while (al2.size() > 0) {
+                    result.addLast(al2.removeFirst());
                 }
                 return result;
             }
             if (al2.size() == 0 ) {
-                while (al1.size() != 0 ) {
-                    result.add(al1.remove(0));
+                while (al1.size() > 0) {
+                    result.addLast(al1.removeFirst());
                 }
                 return result;
             }
-            if (al1.get(0) <= al2.get(0)) {     //比较两组第一个数的大小,把小值加到结果result集合
-                result.add(al1.remove(0));
+            if (al1.getFirst() <= al2.getFirst()) {     //比较两组第一个数的大小,把小值加到结果result集合
+                result.addLast(al1.removeFirst());
             } else {
-                result.add(al2.remove(0));
+                result.addLast(al2.removeFirst());
             }
         }
         return result;
